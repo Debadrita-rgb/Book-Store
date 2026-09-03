2a4c36fc16fbe6e61cbaa50f734eacc91e290e6e
Debadrita-rgb <debadritapaul76@gmail.com>
2026-09-03 18:30:39 +0530

    change config and server file

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      // "https://show-hub-frontend.onrender.com",
+      "https://book-store-frontend-2pz4.onrender.com/",
     ],
     credentials: true,
   }),
